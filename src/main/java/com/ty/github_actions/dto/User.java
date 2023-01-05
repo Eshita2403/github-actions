ddbc943666c341fa8ac7dd4c6c07da41e4fd39a1
Eshita2403 <eeshitamahant@gmail.com>
2023-01-05 11:47:40 +0530

    initial commit

---
+++ src/main/java/com/ty/github_actions/dto/User.java
@@ -0,0 +1,21 @@
+package com.ty.github_actions.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "USER_TBL")
+public class User {
+	@Id
+	private int id;
+	private String username;
+	private String password;
+}
